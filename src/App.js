d41611241420f6967f166a8d90a3c0e8439a0b60
Sergey Tychenko <seregio2002@outlook.com>
2022-05-11 11:16:25 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import SmthTable from './Table';
+import logo from "./logo.svg";
+import "./App.css";
+import SmthTable from "./Table";
+import moment from "moment";
+import "moment/locale/ru";
+moment.locale("ru");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <body className='App-body'>
-        <SmthTable/>
-      </body>
-      <footer className="App-footer">
-        <p> Сделано <a className='App-Link' href="https://ya.ru" target="_blank" rel="noreferrer noopener">The Real Puffs</a></p>
-        <p> Чьи-то права точно защищены</p>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>СибГУТИ расписание</p>
+                <div className="App-today">
+                <p>
+                    Сегодня {moment().format("ll")},{" "}
+                    {moment().format("dddd")}
+                </p>
+                <p className="Week_2"> 10 учебная неделя</p>
+
+                </div>
+            </header>
+            <body className="App-body">
+                <SmthTable />
+            </body>
+            <footer className="App-footer">
+                <p>
+                    {" "}
+                    Сделано{" "}
+                    <a
+                        className="App-Link"
+                        href="https://ya.ru"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        The Real Puffs
+                    </a>
+                </p>
+                <p> Чьи-то права точно защищены</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
