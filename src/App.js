fc8f1ef1fef7dbb5b07bdf6c9fd0008c978f69b1
Sergey Tychenko <seregio2002@outlook.com>
2022-05-11 10:56:18 +0700

    update height

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <body>
+      <body className='App-body'>
         <SmthTable/>
       </body>
       <footer className="App-footer">
