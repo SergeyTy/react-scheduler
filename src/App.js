a1436476cc40b2955189738afe1ff053eb719a5d
Sergey Tychenko <seregio2002@outlook.com>
2022-05-13 09:50:41 +0700

    add smart week

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import SmthTable from "./Component/Table";
 import moment from "moment";
 import "moment/locale/ru";
 import MyInput from "./Component/UI/MyInput";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 moment.locale("ru");
+
+const data = ["0","01","02","03"]
 
 function App() {
     const bodyInputRef = useRef();
+    
+    const [CurrentWeek, setCurrentWeek] = useState(moment().week());
+    
+
+
+    
+    const [Title, setTitle] = useState("0");
 
     const show = (e) => {
         console.log(bodyInputRef.current.value);
+        setTitle(bodyInputRef.current.value);
     };
 
     return (
@@ -35,7 +45,13 @@ function App() {
                                 {moment().format("ll")},{" "}
                                 {moment().format("dddd")}
                             </p>
-                            <p>12 неделя</p>
+                            {
+                            CurrentWeek % 2 !==0 
+                            ?
+                            <p className="Week_1">{CurrentWeek} учебная неделя</p>
+                            :
+                            <p className="Week_2">{CurrentWeek} учебная неделя</p>
+                        }
                         </div>
                     </div>
                     <div className="Search">
@@ -48,8 +64,9 @@ function App() {
                     </div>
                 </div>
             </header>
-            <body>
-                <SmthTable />
+            <body className="App-body">
+                <p>{Title}</p>
+                <SmthTable {...data} />
             </body>
             <footer className="App-footer">
                 <p>
