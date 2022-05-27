c956759fc3100fd3cc677f6121e29621428f93a4
Gleb Plaxienko <gleb.plaxienko@gmail.com>
2022-05-27 11:03:08 +0700

    Fixed temperature counter

--- src/Component/Weather/Weather.jsx
+++ src/Component/Weather/Weather.jsx
@@ -6,13 +6,14 @@ import './Weather.module.css';
 const Weather = ({weatherData}) => (
     <div className="Weather">  
         <h4>Wheather Data: {weatherData.temp}</h4>
-        <h3>Temprature: {weatherData.main.temp} &deg;C</h3>
+        <h3>Temperature: {Math.round(weatherData.main.temp - 273)} &deg;C</h3>
         <h3>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</h3>
         <h3>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</h3>
         <h3>Description: {weatherData.weather[0].main}</h3>
         <h3>Humidity: {weatherData.main.humidity} %</h3> 
-        <h3>Feels: {weatherData.main.feels_like} &deg;C</h3> 
+        <h3>Feels: {Math.round(weatherData.main.feels_like - 273)} &deg;C</h3> 
         <h3>City Name: {weatherData.name}</h3>
+        <h5 style={{"text-align": "left", "margin": "1em 0 0 0"}}>by Pasha</h5>
     </div>
 )
 
