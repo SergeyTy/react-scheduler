b6986dc97539147f891c08830c412a6bfd6dd6b6
Sergey Tychenko <seregio2002@outlook.com>
2022-05-27 10:09:36 +0700

    update scheduler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
-import SmthTable from "./Component/Table";
 import moment from "moment";
 import "moment/locale/ru";
 import { useMemo, useRef, useState, useEffect  } from "react";
 import ItemData from "./data/ItemData";
-import { ri01, ri02 } from "./data/ri-0";
-import ErrorBoundary from "./Component/ErrorBoundary";
-import MyCombox from "./Component/MyCombox";
+import { ri02 } from "./data/ri-0";
+
+//import SmthTable from "./Component/Table";
+//import ErrorBoundary from "./Component/ErrorBoundary";
+//import MyCombox from "./Component/MyCombox";
+//import { Search } from "./Search";
+
 import { Today } from "./Component/Today";
 import { Plock } from "react-plock";
 
 import Weather from './Component/Weather/Weather';
 import MySwipe from "./Component/MySwipe";
 
-import { Search } from "./Search";
 
 moment.locale("ru");
 //require(`dotenv`).config();
@@ -24,31 +26,25 @@ function App() {
         moment().week() - moment("02-01-2022", "MM-DD-YYYY").week()+1
     );
 
-    const [CurrentSch, setCurrentSch] = useState();
-
-    const [SearchQuery, setSearchQuery] = useState();
-
-    const [FilteredItem, setFilterdItem] = useState([]);
-
-    const SearchedScheduler = useMemo(() => {
-        if (SearchQuery !== null) {
-            return ItemData.filter((item) =>
-                item.item
-                    .toLowerCase()
-                    .includes(SearchQuery)
-            );
-        } else return null;
-    }, [SearchQuery, ItemData]);
-
-    const listItems = ItemData.map((i) => (
-        <p key={i.key}> {i.item} </p>
-    ));
-
-    const [Title, setTitle] = useState("0");
-
-    const show = (e) => {
-        setTitle(bodyInputRef.current.value);
-    };
+    // const [CurrentSch, setCurrentSch] = useState();
+    // const [FilteredItem, setFilterdItem] = useState([]);
+    // const [SearchQuery, setSearchQuery] = useState();
+    // const SearchedScheduler = useMemo(() => {
+    //     if (SearchQuery !== null) {
+    //         return ItemData.filter((item) =>
+    //             item.item
+    //                 .toLowerCase()
+    //                 .includes(SearchQuery)
+    //         );
+    //     } else return null;
+    // }, [SearchQuery, ItemData]);
+    // const listItems = ItemData.map((i) => (
+    //     <p key={i.key}> {i.item} </p>
+    // ));
+    // const [Title, setTitle] = useState("0");
+    // const show = (e) => {
+    //     setTitle(bodyInputRef.current.value);
+    // };
 
     const breakpoints = [
         { size: 640, columns: 1 },
@@ -70,16 +66,16 @@ function App() {
             //      NSK
             // lat = 55.0415 
             // long = 82.9346 
-											//	http://api.openweathermap.org/data/2.5/find?q=Petersburg&type=like&APPID=6d8e495ca73d5bbc1d6bf8ebd52c4
+											//	https://api.openweathermap.org/data/2.5/find?q=Petersburg&type=like&APPID=6d8e495ca73d5bbc1d6bf8ebd52c4
 											//	${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}
 											//	https://api.openweathermap.org/data/2.5/weather/?lat=55.05&lon=44.05&units=metric&APPID=59aa82de2145172e26222d8b6415223c
-											//http://api.openweathermap.org/data/2.5/find?lat=55.05&lon=44.05&APPID=59aa82de2145172e26222d8b6415223c
-											//http://api.openweathermap.org/data/2.5/weather?lat=55.0415&lon=82.9346&APPID=59aa82de2145172e26222d8b6415223c
+											//https://api.openweathermap.org/data/2.5/find?lat=55.05&lon=44.05&APPID=59aa82de2145172e26222d8b6415223c
+											//https://api.openweathermap.org/data/2.5/weather?lat=55.0415&lon=82.9346&APPID=59aa82de2145172e26222d8b6415223c
 			console.log("lat = ", lat, " long = ", long)
             console.log("url = ",process.env.REACT_APP_API_URL)
             
             console.log("key = ",process.env.REACT_APP_API_KEY)
-			await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=59aa82de2145172e26222d8b6415223c`)
+			await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=54.9912576&lon=82.968576&APPID=59aa82de2145172e26222d8b6415223c`)
 			.then(res => res.json())
 			.then(result => {
 				setData(result)
@@ -111,18 +107,12 @@ function App() {
                     <Today CurrentWeek={CurrentWeek} />
                 </div>
 
-                <div className="WeekButtons">
-                    <h4>Неделя:</h4>
-                    <button>Нечётная</button>
-                    <button>Чётная</button>
-                </div>
-
+                <div></div>
                 <div> {
                     (typeof data.main != 'undefined') ? (
                     <Weather weatherData={data}/>): (
-                    <div></div>)
+                    <div>Здесь могла быть погда</div>)
                 }</div>
-
                 <MySwipe data={ri02.data[CurrentWeek % 2]} style={{width: "100% !imortant"}}/>
             </Plock>
             {/* <div className="Header-content">
